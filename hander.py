050c2528d04d6919ea7d444e389cab87993c9d01
NXZ02 <109139829+NXZ02@users.noreply.github.com>
2024-05-22 09:01:01 +0700

    Update hander.py

--- hander.py
+++ hander.py
@@ -3,10 +3,11 @@ from tkinter import *
 from PIL import Image, ImageTk # import lib 
 import os
 import tkinter.messagebox
+from customtkinter import *
 ################################
 
 # config 
-root = Tk()
+root = CTk()
 # size windows
 root.geometry("600x500")
 
@@ -34,7 +35,7 @@ lab=Label(image=photo) .pack()  # center
 
 ############################
 def install_pass():
-    tkinter.messagebox.showinfo("Message","Install Finish \nreboot you computer ")
+    tkinter.messagebox.showinfo("Message","Install Finish reboot you computer ")
 ############################
 
 logo_img = Image.open("logo.png")
@@ -45,71 +46,81 @@ logo_label.place(x=520, y=10)  # Adjust position as needed
 
 
 #######################################################
-button_img = Image.open("next1.png")
-resize_button_img = button_img.resize((150, 45))
-button_photo = ImageTk.PhotoImage(resize_button_img)
+#button_img = Image.open("next1.png")
+#resize_button_img = button_img.resize((150, 45))
+#button_photo = ImageTk.PhotoImage(resize_button_img)
 ###########################3#########################
 
 
 ########################################### create frame gui
 frame = Frame(root, bd=2, relief="groove")
-frame.place(x=80, y=110)
+frame.place(x=80, y=100)
 ############################################
 
 Head = Label(root,text="NVIDIA Graphics Driver",fg="green",font=("Helvetica", 12)) .place(x=5,y=5)
 lowHead = Label(root,text="Version Arch Linux",fg="green",font=("Helvetica", 12)) .place(x=5,y=30)
 
-TextFrame = Label(frame, text="LIST NVIDIA Graphics Driver", fg="green", font=("Helvetica", 12))
+TextFrame = Label(frame, text=" NVIDIA Graphics Driver", fg="green", font=("Helvetica", 12))
 TextFrame.pack(padx=30,pady=40)
 
 TextFrame1 = Label(frame, text="            ", fg="green", font=("Helvetica", 12))
-TextFrame1.pack(padx=30,pady=80)
+TextFrame1.pack(padx=30,pady=100)
 
 #create_button = Button(root,text="HELLO") .place(x=90,y=60)
 #create_button = Button(root, image=button_photo) .place(x=400,y=400)
 #create_exit = Button(root,text="EXIT",command=Cilck_exit) .place(x=300,y=425)
 
+def Check():
+    tkinter.messagebox.askquestion("Message","Confirm Yes/No")
 
-def show(): # funtion choice nvidia driver 
-    r = brave.get()
-    if r == 1:
+def INSTALL(): # funtion choice nvidia driver 
+    r = int(text.get())
+    r2 = int(text2.get())
+    r3  = int(text3.get()) 
+    r4 = int(text4.get())
+    r5 = int(text5.get())
+    SUM = r + r2 + r3 + r4 + r5
+    if SUM != 1:
+        tkinter.messagebox.showerror("Message ERROR","Please select only 1 option.")
+    elif r == 1:
+        Check()
         root.destroy()
         os.system("sudo pacman -S base-devel linux-headers git nano --needed --noconfirm")
         os.system("yay -S nvidia-340xx-dkms nvidia-340xx-utils lib32-nvidia-340xx-utils nvidia-settings --noconfirm")
-        os.system("clear;echo reboot you computer;echo ")
         install_pass()
-    elif r == 2:
+    elif r2 == 2:
+        Check()
         root.destroy()
         os.system("sudo pacman -S base-devel linux-headers git nano --needed --noconfirm")
         os.system("yay -S nvidia-390xx-dkms nvidia-390xx-utils lib32-nvidia-390xx-utils nvidia-settings --noconfirm")
-        os.system("clear;echo reboot you computer;echo ")
         install_pass()
-        
-    elif r == 3:
+    elif r3 == 3:
+        Check()
         root.destroy()
-        os.system("sudo pacman -S git")
         os.system("sudo pacman -S base-devel linux-headers git nano --needed --noconfirm")
         os.system("yay -S nvidia-470xx-dkms nvidia-470xx-utils lib32-nvidia-470xx-utils nvidia-settings --noconfirm")
-        os.system("clear;echo reboot you computer;echo ")
         install_pass()
-    elif r == 4:
+    elif r4 == 4:
+        Check()
         root.destroy()
         os.system("sudo pacman -S base-devel linux-headers git nano --needed --noconfirm")
         os.system("yay -S nvidia-525xx-dkms nvidia-525xx-utils lib32-nvidia-525xx-utils nvidia-settings --noconfirm")
-        os.system("clear;echo reboot you computer;echo ")
         install_pass()
-    elif r == 5:
+    elif r5 == 5:
+        Check()
         root.destroy()
         os.system("sudo pacman -S base-devel linux-headers git nano --needed --noconfirm")
         os.system("yay -S nvidia-535xx-dkms nvidia-535xx-utils lib32-nvidia-535xx-utils nvidia-settings --noconfirm")
-        os.system("clear;echo reboot you computer;echo ")
         install_pass()
-    else:
-        exit()
 
 
+    
 
-brave = IntVar()
+text = IntVar()
+text2 = IntVar()
+text3 = IntVar()
+text4 = IntVar()
+text5 = IntVar()
 
 xin = 90 # x = 90
 
@@ -119,11 +130,12 @@ xin = 90 # x = 90
  #                                * NVIDIA GRAPHICS DRIVER   * 
  #
 ##################################################################################################
-Radiobutton(text="Driver version : 340xx",variable=brave,value=1,command=show) .place(x=xin,y=200) # choice 1
-Radiobutton(text="Driver version : 390xx",variable=brave,value=2,command=show) .place(x=xin,y=230) # choice 2
-Radiobutton(text="Driver version : 470xx",variable=brave,value=3,command=show) .place(x=xin,y=260) # choice 3
-Radiobutton(text="Driver version : 525xx",variable=brave,value=4,command=show) .place(x=xin,y=290) # choice 4
-Radiobutton(text="Driver version : 535xx",variable=brave,value=5,command=show) .place(x=xin,y=320) # choice 5
+CTkCheckBox(master=root, text="340xx",fg_color="green",corner_radius=36, onvalue=1,offvalue=0,variable=text) .place(relx=0.3, rely=0.4 ,anchor="center")
+CTkCheckBox(master=root, text="390xx",fg_color="green",corner_radius=36, onvalue=1,offvalue=0,variable=text2) .place(relx=0.3, rely=0.5 ,anchor="center")
+CTkCheckBox(master=root, text="470xx",fg_color="green",corner_radius=36, onvalue=1,offvalue=0,variable=text3) .place(relx=0.3, rely=0.6 ,anchor="center")
+CTkCheckBox(master=root, text="525xx",fg_color="green",corner_radius=36, onvalue=1,offvalue=0,variable=text4) .place(relx=0.3, rely=0.7 ,anchor="center")
+CTkCheckBox(master=root, text="535xx",fg_color="green",corner_radius=36, onvalue=1,offvalue=0,variable=text5) .place(relx=0.3, rely=0.8 ,anchor="center")
+CTkButton(master=root, text="Install",border_color="black",fg_color="green",border_width=2, hover_color="black",command=INSTALL) .place(x=450,y= 400)
 ##################################################################################################
 
 ##########################################   END #################################################
